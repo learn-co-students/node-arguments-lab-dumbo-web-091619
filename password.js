49093ebb3d68411b1b1861485078e9a9f9cf6531
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-03-13 16:34:45 -0400

    Done.

--- password.js
+++ password.js
@@ -6,7 +6,6 @@ if (process.argv.length !== 4){
 let makePassword = (passwordLength) => {
     let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+'
     let password=""
-
     // add random character to password string until desired length is achieved
     for (let i=1; i <= passwordLength; i++){
         password += characters.charAt(Math.floor(Math.random() * characters.length))
@@ -14,26 +13,8 @@ let makePassword = (passwordLength) => {
       return password
 }
 
-// this function collects and prints passwords the desired number of times
-let collectAndPrintPasswords = (passwordLength, numberOfPasswordsNeeded) => {
-    
-    // If only one password is needed, make & print it.
-    if (numberOfPasswordsNeeded === 1){
-        console.log(makePassword(passwordLength))
-    } 
-    
-    // if more than one password is needed, collect them in an array and print all.
-    let passwords=[]
-    let num = 1
-    if (num <= numberOfPasswordsNeeded){
-        num++
-        passwords.push(makePassword(passwordLength))
+    for (let i = 1; i<= process.argv[3]; i++){
+        console.log(makePassword(process.argv[2]))
     }
-  passwords.forEach(password => console.log(password))
-}
-
-// this passes the node CLI arguments (which will be after the node.exe & path aka argv[0] & argv[1]) to & runs the function to create & print all desired passwords.
-collectAndPrintPasswords(process.argv[2], process.argv[3])
-
 // done
 process.exit(0)
